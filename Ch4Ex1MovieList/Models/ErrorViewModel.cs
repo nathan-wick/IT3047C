6fe1663a8f99503a79442cfa6d6eb76a6bd16942
Nathan Wick <nathanwick2001@gmail.com>
2022-02-10 15:34:03 -0500

    Restarted Assignment 1

--- Ch4Ex1MovieList/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Ch4Ex1MovieList.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
