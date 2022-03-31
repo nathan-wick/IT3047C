b69b7cb29215392595e0118b446e98bbd1b2df41
Nathan Wick <nathanwick2001@gmail.com>
2022-03-31 16:30:20 -0400

    Lab 4

---
+++ Assignment4/Ch11Ex1TempManager/TempManager/Controllers/ValidationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using TempManager.Models;
+
+public class ValidationController : Controller
+{
+    private TempManagerContext data { get; set; }
+    public ValidationController(TempManagerContext ctx) => data = ctx;
+
+    public JsonResult CheckDate(string date)
+    {
+        var temp = data.Temps.FirstOrDefault(c => c.Date == DateTime.Parse(date));
+        if (temp == null)
+        {
+            return Json(true);
+        }
+        else return Json("Date already exists");
+    }
+}
---
+++ Assignment4/Ch11Ex1TempManager/TempManager/Models/Temp.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TempManager.Models
+{
+    public class Temp
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Remote("CheckDate", "Validation")]
+        public DateTime? Date { get; set; }
+
+        [Required]
+        [Range(-200, 200)]
+        public double? Low { get; set; }
+
+        [Required]
+        [Range(-200, 200)]
+        public double? High { get; set; }
+    }
+}
